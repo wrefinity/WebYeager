520349ecdf4cba937ca8a936a6e0aa1d465df3ba
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-06-14 16:10:26 -0500

    menu and sub-events for htmx

--- yeager_ai/yeager_ai/world/consumers.py
+++ yeager_ai/yeager_ai/world/consumers.py
@@ -8,6 +8,9 @@ logger = logging.getLogger(__name__)
 FastAPI connect
 '''
 class FastAPIConsumer(AsyncWebsocketConsumer):
+    
+    grouped_events = {}
+    
     async def connect(self):
         self.channel_world = 'world'
         await self.channel_layer.group_add(self.channel_world, self.channel_name)
@@ -30,21 +33,29 @@ class FastAPIConsumer(AsyncWebsocketConsumer):
         
         
         
+
     async def world_event(self, event):
         data = event['data']
-        logger.info('===Received message=====')
-
         try:
-            if 'nearby_entities' in data:
-                template_name = "partials/Events/agent_event.html"
-                html = get_template(template_name).render(context={"data": data})
-                
-                await self.send(text_data=html)
-            else:
-                template_name = "partials/Events/event.html"
-            
-                html = get_template(template_name).render(context={"data": data})
-                await self.send(text_data=html)
+            if "sender_id" in data:
+                data_key = data['sender_id']
+                if data_key in self.grouped_events:
+                    self.grouped_events[str(data_key).lower()].append(data)
+                    json_data = json.dumps({"groupedEvents": self.grouped_events})
+                    await self.send(text_data=json_data)  # Send JSON data through WebSocket
+                else:
+                    self.grouped_events[str(data_key).lower()] = [data]
+
+            template_name = "partials/Events/agent_event.html"
+            html = get_template(template_name).render(context={"grouped_events": self.grouped_events})
+
+            await self.send(text_data=html)
+
+            # Send the data without filtering
+            template_name = "partials/Events/event.html"
+            html = get_template(template_name).render(context={"data": data})
+            await self.send(text_data=html)  # Send JSON data through WebSocket
+
         except Exception as e:
             print("An error occurred while sending the data:", e)
 
